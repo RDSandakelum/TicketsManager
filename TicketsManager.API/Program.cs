f4134909331331d7020a274e7cd818951d75fcba
Dinushka Ramanayaka <dinushka.sandakelumr@gmail.com>
2025-07-08 14:09:58 +0530

    Cleanup v2

--- TicketsManager.API/Program.cs
+++ TicketsManager.API/Program.cs
@@ -1,20 +1,7 @@
-using Microsoft.EntityFrameworkCore;
-using TicketsManager.Business.SecurityService;
-using TicketsManager.Business.UserManagement;
-using TicketsManager.DataAccess.Repositories;
-using TicketsManager.DataAccess.TicketManagerDbContext;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddScoped<ISecurityService, SecurityService>();
-builder.Services.AddScoped<IUserManagement, UserManagement>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddDbContext<TicketsManagerDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 
 var app = builder.Build();
 
--- TicketsManager.Business/Result.cs
+++ TicketsManager.Business/Result.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TicketsManager.Business;
+﻿namespace TicketsManager.Business;
 
 public class Result<T>
 {
